48942cf2d8e7e89626e3eab7d172b1e8c12f9f89
Favour <favourijeoma219@gmail.com>
2022-09-09 15:42:04 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - This program will assign a random number to the variable n
- * each time it is executed.
- * Complete the source code in order to print the last digit of
- * the number stored in the variable n.
+ * main - Entry point
+ * author: Favour Johnson
+ * date:09-09-2022
  * Return: 0 (success)
  */
 int main(void)
@@ -18,8 +17,8 @@ int main(void)
 	if (n % 10 > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	else if (n % 10 == 0)
-		printf("Last digit of %d is %d is %d and is 0\n", n, n % 10);
-	else if (n % 10 < 6 && n % 10 != 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else if ((n % 10 < 6) && n % 10 != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	return (0);
 }
