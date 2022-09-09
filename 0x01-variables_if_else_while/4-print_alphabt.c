26e3c31c1be0c448116ceb3bf81a024557c44865
Favour <favourijeoma219@gmail.com>
2022-09-09 16:31:58 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: favour
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char favvy;
+
+	for (favvy = 'a'; favvy <= 'z'; sammy++)
+	{
+		if (favvy != 'q' && favvy != 'e')
+			putchar(favvy);
+	}
+	putchar('\n');
+	return (0);
+}
