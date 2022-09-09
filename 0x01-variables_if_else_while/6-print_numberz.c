13ac53476caaf8f85730fbefed19fece675cafbd
Favour <favourijeoma219@gmail.com>
2022-09-09 16:58:17 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: favvy
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int favvy;
+
+	for (favvy = 48; favvy < 58; favvy++)
+		putchar(favvy);
+	putchar('\n');
+	return (0);
+}
