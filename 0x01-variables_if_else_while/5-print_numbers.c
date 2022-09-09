01193144bd809c9827696c60479384beacd55066
Favour <favourijeoma219@gmail.com>
2022-09-09 16:51:17 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: favvy
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int favvy;
+
+	for (favvy = 0; favvy < 10; favvy++)
+	{
+		printf("%d", favvy);
+	}
+	printf("\n");
+	return (0);
+}
