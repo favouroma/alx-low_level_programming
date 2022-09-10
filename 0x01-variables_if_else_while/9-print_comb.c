a331d2524ea09ccfa9d5de34de9bfb646f87bcf0
Favour <favourijeoma219@gmail.com>
2022-09-09 17:18:37 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * autor: favvy
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int favvy;
+	char john;
+
+	for (favvy = 48; favvy < 58; favvy++)
+		putchar(john);
+	putchar('\n');
+	return (0);
+}
