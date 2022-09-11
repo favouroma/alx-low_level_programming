de4b3bf3fd94c214eb10d2b9a108bb38cd70a76f
Favour <favourijeoma219@gmail.com>
2022-09-11 13:53:31 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string>
 
 /**
- * main - Entry point
- * author: favour
+ * main - Prints the alphabet without q and e.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char favvy;
+	int i;
 
-	for (favvy = 'a'; favvy <= 'z'; favvy++)
+	for (i = 97; i < 123; i++)
 	{
-		if (favvy != 'q' && favvy != 'e')
-			putchar(favvy);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
-	putchar('\n');
+	putchar('\n);
 	return (0);
 }
