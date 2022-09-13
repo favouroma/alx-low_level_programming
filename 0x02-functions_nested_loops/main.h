d3985d2a36502341d9dd0ebacdf51d57dc4f2b5f
Favour <favourijeoma219@gmail.com>
2022-09-13 12:35:46 -0700

    Updated repo with the c executable file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_10x(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_tables(int n);
+
+#endif
