c16c74580a32779f4b3d102f8c3215fe355f7801
Favour <favourijeoma219@gmail.com>
2022-09-13 15:40:25 -0700

    C function that prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print 10 times the alphabet, in lowercase.
+ * print_alphabet_10x - Print 10 times the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)
+void print_alphabet_10x(void)
 {
 	int favour, b;
 
