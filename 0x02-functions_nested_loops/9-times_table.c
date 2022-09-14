dbb9dbeeaf3ec724a6d428ceac592c1a1e145fef
Favour <favourijeoma219@gmail.com>
2022-09-14 06:46:00 -0700

    9 times table using function

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int jasper, alx, op;
+
+	for (jasper = 0; jasper <= 9; alx++)
+	{
+		_putchar(48);
+		for (alx = 1; alx <= 9; alx++)
+		{
+			op = jasper * alx;
+			_putchar (44);
+			_putchar(32);
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
