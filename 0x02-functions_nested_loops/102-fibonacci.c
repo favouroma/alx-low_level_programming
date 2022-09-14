97bbce84a5b665dc0e4907b3772cfd44fde50c4a
Favour <favourijeoma219@gmail.com>
2022-09-14 08:36:00 -0700

    Fibonacci series Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the add of the Fibonacci numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+	long int n1, n2, fn;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%1d, %1d", n1, n2);
+	for (c = 0; c < 48; c++)
+	{
+		fn = n1 + n2;
+		printf(', %1d", fn);
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("\n");
+	return (0);
+}
