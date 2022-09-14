4842dad8be03feeeb81b4e60e004493440e36695
Favour <favourijeoma219@gmail.com>
2022-09-14 06:52:45 -0700

    updated 9 times table using function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,11 +27,6 @@ void times_table(void)
 				_putchar((op / 10) + 48);
 				_putchar((op % 10) + 48);
 			}
-			else
-			{
-				_putchar((op / 10) + 48);
-				_putchar((op % 10) + 48);
-			}
 		}
 		_putchar('\n');
 	}
