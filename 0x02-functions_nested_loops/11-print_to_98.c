4ab306cb648d74a8950fc9d0c35be1ab042c420d
Favour <favourijeoma219@gmail.com>
2022-09-14 07:33:55 -0700

    Updated print all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,19 +10,19 @@
  */
 void print_to_98(int n)
 {
-	if (n >98)
+	if (n > 98)
 	{
 		for (; n > 98; n--)
 		{
-			printf(%d, ", n);
+			printf(" % d", ", n);
 		}
 	}
 	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
-			printf('%d", ", n);
+			printf(" % d", ", n);
 		}
 	}
-	printf("%d\n", n);
+	printf(" % d\n", n);
 }
