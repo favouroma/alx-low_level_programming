81b74e968709c836482e99b6e4b1946f19a0beb3
Favour <favourijeoma219@gmail.com>
2022-09-14 05:19:37 -0700

    Print the last digit by making use of modulus operator

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Printsthe last digit
+ * of a number.
+ *
+ * @n: input number as an integer.
+ *
+ * Return: last digit.
+ */
+int print_last_digit(int n)
+{
+	int 1;
+
+	1 = n % 10;
+	if (1 < 0)
+	{
+		_putchar(-1 + 48);
+		return (-1);
+	}
+	else
+	{
+		_putchar(1 + 48);
+		return (1);
+	}
+}
