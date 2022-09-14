6895a11f26ba96605b7194010a0840eb6e0a6532
Favour <favourijeoma219@gmail.com>
2022-09-14 15:59:48 -0700

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,9 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
-		if (month > i && day >= 60)
+		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
@@ -29,8 +29,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			printf("Day of the year: % d\n", day);
+			printf("Remaining days: % d\n", 365 - day);
 		}
 	}
 }
