0e8ce09239f135de22a8de086f8e14bd8ac1403b
Favour <favourijeoma219@gmail.com>
2022-09-14 15:31:11 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,9 +11,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
-		if (month >= 3 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_numbers(int a, int b, int c,);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-void print_remainig_days(int month, int day, int year);
 
 #endif /* MAIN_H */
