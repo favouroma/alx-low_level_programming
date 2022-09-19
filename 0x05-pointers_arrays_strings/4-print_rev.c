1c3e8b0a36bdb0204648adf701dba806ba00fd98
Favour <favourijeoma219@gmail.com>
2022-09-19 14:32:46 -0700

    updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,6 +5,7 @@
  * @s: the used string reference pointer
  * Return: 0
  */
+
 void print_rev(char *s)
 {
 	int i = 0;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+int _atoi(char *s);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,4 +10,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+
+
