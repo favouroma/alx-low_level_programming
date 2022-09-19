5597592fde4e682f5128c84351a0f9bf7055f57e
Favour <favourijeoma219@gmail.com>
2022-09-19 12:43:07 -0700

    completed task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - This function swaps the value of two integers
+ * @a: a pointer deference for changing the value of the variable a
+ * @b: a pointer deference for changing the value of the variable b
+ * int c: assigning the address of the variable a to c
+ *
+ * Return: 0 success.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
