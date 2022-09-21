775e1966030a1872891ff1ad8a2cd6f59554a83f
Favour <favourijeoma219@gmail.com>
2022-09-21 12:01:02 -0700

    completed task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: A pointer to a character that will be changed
+ * @src: A pointer to a character that will be changed
+ * @n: value
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		desti[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+
+}
