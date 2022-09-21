b0198add7cb6a73e60a472f0394dfb3dbcdda9b1
Favour <favourijeoma219@gmail.com>
2022-09-21 12:08:59 -0700

    updated task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,8 +15,14 @@ char *_strncpy(char *dest, char *src, int n)
 	i = 0;
 	while (i < n && src[i] != '\0')
 	{
-		desti[i] = src[i];
+		dest[i] = src[i];
 		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++
 	}
 
 	return (dest);
