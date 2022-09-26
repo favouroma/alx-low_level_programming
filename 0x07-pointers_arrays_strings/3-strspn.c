9023975e6372fadf1262885e5f09ee2193b884b3
Favour <favourijeoma219@gmail.com>
2022-09-26 15:25:38 -0700

    completed task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,14 +16,20 @@ unsigned int _strspn(char *s, char *accept)
 
 		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
 		{
-			matches++;
-			break;
+			if (s[i] == accept[j]) /*record & break at first match*/
+			{
+				matches++;
+				break;
+			}
+
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+
+				return (matches) ;/*return if idx doesn't match*/
 		}
 
-		if (accept[j + 1] == '\0' && s[i] != accept[j])
-
-		return (matches);/*return if idx doesn't match*/
+		i++;
 	}
 
-	i++;
+	return (matches); /* return num if all match till end */
+
 }
