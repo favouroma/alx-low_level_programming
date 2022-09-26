1679dd27cc736ed93f7a5ccdfaf29c3da2502c87
Favour <favourijeoma219@gmail.com>
2022-09-26 16:42:28 -0700

    completed task 7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,7 +15,7 @@ int _putchar(char c);
 
 	char *_strstr(char *haystack, char *needle);
 
-	void print_chessboard(char (a)[8]);
+	void print_chessboard(char (*a)[8]);
 
 	void print_diagsums(int *a, int size);
 
