bbb5c153adc61f551e105d68f648198a9e580006
Favour <favourijeoma219@gmail.com>
2022-09-26 16:05:39 -0700

    completed task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,11 +9,10 @@ void print_chessboard(char (*a)[8])
 	int row;
 	int column;
 
-
 	for (row = 0; row < 8; row++)
 	{
 		for (column = 0; column < 8; column++)
-		_putchar(a[row][column]);
+			_putchar(a[row][column]);
 		_putchar('\n');
 	}
 }
