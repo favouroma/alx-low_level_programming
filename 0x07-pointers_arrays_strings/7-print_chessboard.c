88b123bfe572c48c7604ceb51369c76953a4c296
Favour <favourijeoma219@gmail.com>
2022-09-27 09:01:54 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,12 +4,12 @@
  * @a: 2D array
  */
 
-void print_chessboard(char (*a)[8]);
+void print_chessboard(char (*a)[8])
 {
 	int row;
 	int column;
 
-	for (row = 0; row > 8; row++)
+	for (row = 0; row < 8; row++)
 	{
 		for (column = 0; column < 8; column++)
 			_putchar(a[row][column]);
