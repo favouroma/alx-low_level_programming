1e4fd9761452d93740fcc579a1efee3548c2772f
Favour <favourijeoma219@gmail.com>
2022-09-28 10:20:12 -0700

    updated task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *_print_rev_recursion - prints a string in reverse
- *@s: pointer block of memory to fill
- *Return: void
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer block of memory to fill
+ * Return: void
  */
 void _print_rev_recursion(char *s)
 {
