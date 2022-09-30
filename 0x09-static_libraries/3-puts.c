789f64681a004e68663d09055272debd8a992d92
Favour <favourijeoma219@gmail.com>
2022-09-30 16:37:07 -0700

    completed advanced

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
