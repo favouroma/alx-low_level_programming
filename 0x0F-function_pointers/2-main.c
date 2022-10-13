f88a364ce6304129bb6836dd409a7aeec5f24b3d
Favour <favourijeoma219@gmail.com>
2022-10-12 21:15:05 -0700

    Write a function that searches for an integer

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -42,12 +42,12 @@ int abs_is_98(int elem)
 int main(void)
 {
 	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-    int index;
+int index;
 
-    index = int_index(array, 20, is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, abs_is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, is_strictly_positive);
-    printf("%d\n", index);
-    return (0);
+index = int_index(array, 20, is_98);
+printf("%d\n", index);
+index = int_index(array, 20, abs_is_98);
+printf("%d\n", index);
+index = int_index(array, 20, is_strictly_positive);
+printf("%d\n", index);
+return (0);
